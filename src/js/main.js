8f9857a0d5dce2bdd604376653fd555a2e444152
romandob <roman.dobrynin@gmail.com>
2016-10-09 18:24:14 +0300

    Added  sort function

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 define(['jquery', 'modalWindow', 'reservationDetails', 'moduleTwo'], function($, ReservationModalWindow, ReservationDetails, ModuleTwo) {
     return $(function() {
+
         var modalWindow, reservationDetails, moduleTwo;
         modalWindow = new ReservationModalWindow();
         reservationDetails = new ReservationDetails();
--- src/js/moduleTwo.js
+++ src/js/moduleTwo.js
@@ -1,14 +1,29 @@
 define(['jquery'], function($) {
-    var moduleTwo;
-    return moduleTwo = (function() {
-        function moduleTwo() {
-            $.getJSON('json/data.json', function(data) {
-                $.each(data, function(i, f) {
-                    console.log(f);
-                });
 
-            });
+
+    function sort_table(tbody, col, asc) {
+        console.log('1');
+        var rows = tbody.rows,
+            rlen = rows.length,
+            arr = new Array(),
+            i, j, cells, clen;
+        // fill the array with values from the table
+        for (i = 0; i < rlen; i++) {
+            cells = rows[i].cells;
+            clen = cells.length;
+            arr[i] = new Array();
+            for (j = 0; j < clen; j++) {
+                arr[i][j] = cells[j].innerHTML;
+            }
         }
-        return moduleTwo;
-    })();
+        // sort the array by the specified column number (col) and order (asc)
+        arr.sort(function (a, b) {
+            return (a[col] == b[col]) ? 0 : ((a[col] > b[col]) ? asc : -1 * asc);
+        });
+        // replace existing rows with new rows created from the sorted array
+        for (i = 0; i < rlen; i++) {
+            rows[i].innerHTML = "<td>" + arr[i].join("</td><td>") + "</td>";
+        }
+    }
+    return sort_table;
 });
